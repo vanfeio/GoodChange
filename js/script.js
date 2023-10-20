c9a08cea78092305183833a012818ffd2cf5ebf1
subaru <vanfeio115@gmail.com>
2023-10-20 21:00:23 +0900

    requireチェック処理

--- js/script.js
+++ js/script.js
@@ -9,90 +9,90 @@ $(function () {
 	$("#search").load("./include/search.html");
 
 	// top - 商品一覧カテゴリタブ
-	$('.tab').on('click', function() {
+	$('.tab').on('click', function () {
 		$('.tab, .panel').removeClass('active');
-	
+
 		$(this).addClass('active');
-		
+
 		var index = $('.tab').index(this);
 		$('.panel').eq(index).addClass('active');
 	});
 
 	// 出品 - 初回モーダルクローズ
-	$(".beginner_modal_close").on("click",function(){
+	$(".beginner_modal_close").on("click", function () {
 		$(".beginner_modal").fadeOut(200);
 	});
 
 	// カテゴリー選択モーダル
 	var open = $('#category_open'),
-	close = $('.category_close'),
-	container = $('.category_area');
+		close = $('.category_close'),
+		container = $('.category_area');
 
-	open.on('click',function(){ 
+	open.on('click', function () {
 		container.addClass('active');
 		return false;
 	});
 
-	close.on('click',function(){  
+	close.on('click', function () {
 		container.removeClass('active');
 	});
 
-	$(document).on('click',function(e) {
-	if(!$(e.target).closest('.category_area .modal_container').length) {
-		container.removeClass('active');
-	}
+	$(document).on('click', function (e) {
+		if (!$(e.target).closest('.category_area .modal_container').length) {
+			container.removeClass('active');
+		}
 	});
-	
+
 	//カテゴリtoggle
-	$('.parent').click(function() {
+	$('.parent').click(function () {
 		$(this).next('ul').slideToggle('fast');
 		$(this).toggleClass('open', 800);
 	});
-	$('.child01').click(function(e) {
+	$('.child01').click(function (e) {
 		$(this).children('ul').slideToggle('fast');
 		$(this).toggleClass('open', 800);
 		e.stopPropagation();
 	});
 
-	$('.sp_toggle_btn').click(function() {
+	$('.sp_toggle_btn').click(function () {
 		$(".sp_toggle").slideToggle('fast');
 	});
 
 	//メッセージ ユーザーネーム横ボタン
-	$('.more_btn').on("click",function(){
+	$('.more_btn').on("click", function () {
 		$(this).next(".more_area").fadeToggle(200);
 	});
 
 	//メッセージ メッセージで取引を開始する
-	$('#start_message').on("click",function(){
+	$('#start_message').on("click", function () {
 		$('#mypage_message .message').fadeIn();
 		$('#mypage_message .message_form').fadeIn();
 		$('#mypage_message .request_btn_area').remove();
 	});
 
-	$('#message_fixed_btn').on("click",function(){
+	$('#message_fixed_btn').on("click", function () {
 		$('#mypage_message .notes_area_fixed').fadeIn();
 		$('#mypage_message .notes_area_completion').remove();
 	});
 
 	// 支払情報の登録 - modal
 	var open = $('#payment_open'),
-	close = $('.payment_open'),
-	container = $('.payment_area');
+		close = $('.payment_open'),
+		container = $('.payment_area');
 
-	open.on('click',function(){ 
+	open.on('click', function () {
 		container.addClass('active');
 		return false;
 	});
 
-	close.on('click',function(){  
+	close.on('click', function () {
 		container.removeClass('active');
 	});
 
-	$(document).on('click',function(e) {
-	if(!$(e.target).closest('.category_area .modal_container').length) {
-		container.removeClass('active');
-	}
+	$(document).on('click', function (e) {
+		if (!$(e.target).closest('.category_area .modal_container').length) {
+			container.removeClass('active');
+		}
 	});
 
 	//続きを読むボタン
@@ -103,24 +103,24 @@ $(function () {
 	var lineHeight = parseFloat($text.css('line-height'));
 	var textNewHeight = lineHeight * lineNum;
 	if (textHeight > textNewHeight) {
-	$text.css({
-		height: textNewHeight,
-		overflow: 'hidden',
-	});
-	$more.click(function () {
-		$(this).hide();
 		$text.css({
-		'height': textHeight,
-		'overflow': 'visible',
+			height: textNewHeight,
+			overflow: 'hidden',
 		});
-		return false;
-	});
+		$more.click(function () {
+			$(this).hide();
+			$text.css({
+				'height': textHeight,
+				'overflow': 'visible',
+			});
+			return false;
+		});
 	} else {
-	$more.hide();
+		$more.hide();
 	}
 
 	//よくあるご質問
-	$("#faq dt").on("click", function() {
+	$("#faq dt").on("click", function () {
 		$(this).next().slideToggle();
 		$(this).toggleClass("active");
 	});
@@ -129,21 +129,37 @@ $(function () {
 	//お問い合わせ - 全部入力するとactive
 	$('#sending').prop("disabled", true);
 
-	$("[id^= input]").change(function () {
-		let send = true;
-		$("[id^= input]").each(function(index) {
-		  if ($("[id^= input]").eq(index).val() === "") {
-			send = false;
-		  }
-		});
-		if (send) {
-			$('#sending').prop("disabled", false);
-		}
-		else {
-			$('#sending').prop("disabled", true);
-		}
+	$("[id^= input][required]").keyup(function () {
+		CheckRequire();
 	});
- 
+	$("[id^= input][required]").change(function () {
+		CheckRequire();
+	});
+
+	function CheckRequire() {
+		let send = true;
+		try {
+			$("[id^= input][required]").each(function (index) {
+				const element = $("[id^= input][required]")[index];
+				//console.log(element)
+				if (element.getAttribute("type") == "checkbox") {
+					if (!element.checked) {
+						send = false;
+						throw "";
+					}
+				} else {
+					if (element.value == "") {
+						send = false;
+						throw "";
+					}
+				}
+			});
+		} catch (e) { }
+		finally {
+			$('#sending').prop("disabled", !send);
+		}
+	}
+
 });
 
 
